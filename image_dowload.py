104b6ba4a2da654a73bf4a23ea6731b072482d01
ih6576 <ih6576_vo@aisin-sw.co.jp>
2020-08-17 18:25:17 +0900

    remove  unuse file

--- image_dowload.py
+++ image_dowload.py
@@ -2,10 +2,6 @@ import wget
 import requests
 import os
 
-# JISB0401-1 
-# https://www.jisc.go.jp/pdfb1/PDFView/ShowPDF/oQIAAMPoeXG1jIZtd-Bs
-# https://www.jisc.go.jp/pdfa7/PDFView/GetImage/hwEAAC2Ky-6M4s5Mruw8?pageNo={}&width=700&seq=0
-
 # B0401-2
 ##url_pattern = "https://www.jisc.go.jp/pdfa8/PDFView/GetImage/FgEAAKUbgM6Lo8notkpb?pageNo={}&width=700&seq=0"
 ## https://img2.sachvui.com/images/2018/conan-2/chap-602-6.jpg
